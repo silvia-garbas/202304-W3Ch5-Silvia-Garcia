5278773e18c799a10a02226bb965752cd8e93971
Silvia <silvia.garbas@gmail.com>
2023-05-19 23:02:43 +0200

    Modified styles.css-index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-new */
+import './styles.css'
+
+import { Header } from './components/header';
+import { Main } from './components/main';
+import { Footer } from './components/footer';
+
+new Header('#app', 'header');
+new Main('#app');
+new Footer('#app', 'pieeeeeeee');
